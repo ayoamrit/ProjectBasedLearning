c71c98b6cc6b9d14e4d241f6e4f02a0158800d09
Amrit <Amrit.09@icloud.com>
2023-10-23 09:43:28 -0700

    Object's Array

---
+++ StudentEnrollment/StudentEnrollment/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentEnrollment;
+
+public class Program
+{
+    static Student[] student = new Student[2];
+    public static void Main(string[] args)
+    {
+        student[0] = new Student(1, "Jerry Seinfeld", 33, "Comedy");
+        student[1] = new Student(2, "George Costanza", 35, "Art & Culture");
+
+        foreach(Student stud in student)
+        {
+            Console.WriteLine(stud.ToString());
+            Console.WriteLine("**********************");
+        }
+    }
+}
---
+++ StudentEnrollment/StudentEnrollment/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment
+{
+    internal class Student
+    {
+        public int studentNumber { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string school { get; set; }
+
+        public Student(int studentNumber, string name, int age, string school)
+        {
+            this.studentNumber = studentNumber;
+            this.name = name;
+            this.age = age;
+            this.school = school;
+        }
+
+        public override string ToString()
+        {
+            return $" Student Number: {studentNumber}\n Student Name: {name}\n Student Age: {age}\n School: {school}";
+        }
+    }
+}
