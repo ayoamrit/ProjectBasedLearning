8c20e8ba77b94b03246f96d15f3496ae9cb9a8b1
Amrit <Amrit.09@icloud.com>
2023-11-02 11:05:51 -0700

    BalancedParentheses Stack

---
+++ BalancedParentheses/BalancedParentheses/Program/Program.cs
@@ -0,0 +1,74 @@
+﻿using BalancedParentheses.Stack;
+
+namespace Program
+{
+    public class Program
+    {
+        static Stack? stack;
+
+        public static void Main(string[] args)
+        {
+            //Define a string containing sequence of brackets
+            string str = "[()()]";
+
+            //Initialize the stack with the capacity of the string length
+            stack = new Stack(str.Length);
+
+            Console.WriteLine(isBalanced(str));
+        }
+
+        //function to check if th string contains balanced parentheses
+        private static bool isBalanced(string str)
+        {
+
+            for(int x = 0; x < str.Length; x++)
+            {
+                char c = str[x];
+
+                if(c == '[' || c == '{' || c == '(')
+                {
+                    //push the opening brackets into the stack
+                    stack.push(c);
+                }
+                else if(c == ']')
+                {
+                    if(stack.peek() != '[')
+                    {
+
+                        //if the stack top does not match the current closing brackets, it's unbalanced
+                        return false;
+                    }
+                    else
+                    {
+                        //pop the matching opening brackets from the stack
+                        stack.pop();
+                    }
+                }
+                else if(c== '}')
+                {
+                    if(stack.peek() != '{')
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        stack.pop();
+                    }
+                }
+                else if (c == ')')
+                {
+                    if(stack.peek() != '(')
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        stack.pop();
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ BalancedParentheses/BalancedParentheses/Stack/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedParentheses.Stack
+{
+    public class Stack
+    {
+        //To init size of the array
+        private int maxSize { get; set; }
+
+        //to store values
+        private char[] chars { get; set; }
+
+        //position of top most element
+        private int top { get; set; }
+
+
+        public Stack(int maxSize)
+        {
+            this.maxSize = maxSize;
+            this.chars = new char[maxSize];
+            this.top = -1;
+        }
+
+        //function to push values onto the stack
+        public void push(char c)
+        {
+            top++;
+            chars[top] = c;
+        }
+
+        //function to remove value from the stack
+        public void pop()
+        {
+            top--;
+        }
+
+        //function to check the value on the top of the stack
+        public char peek()
+        {
+            return chars[top];
+        }
+    }
+}
